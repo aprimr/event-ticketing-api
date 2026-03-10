703dec6c708b5893143e2c642524499961916ef7
aprimr <aprimregmi24@gmail.com>
2026-03-10 20:40:59 +0545

    setup simple web server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aprimr/event-ticketing-api
 
 go 1.25.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env")
+	}
+	mux := http.NewServeMux()
+
+	// SpinUp server
+	port := ":" + os.Getenv("PORT")
+	log.Println("Server started on port", port)
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal("Unexpected error occured")
+	}
 }
